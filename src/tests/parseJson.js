04162efb68e8284366d265a0a07aa221fcbc1dca
Jim McAndrew <jim@loc8.us>
2015-12-24 12:22:32 -0700

    Add tape for tests

--- src/tests/parseJson.js
+++ src/tests/parseJson.js
@@ -1,21 +1,68 @@
+var tape = require('tape');
 var parse = require('../parseJson.js');
 var tests = [{
   a: {
     'test': 'one'
+  },
+  res: {
+    e: undefined,
+    r: {
+      test: 'one'
+    }
   }
 }, {
-  a: '{"test": "one"}'
+  a: '{"test": "one"}',
+  res: {
+    e: undefined,
+    r: {
+      test: 'one'
+    }
+  }
 }, {
-  a: 'test'
+  a: 'test',
+  res: {
+    e: new Error('SyntaxError: Unexpected token µ'),
+    r: undefined
+  }
 }, {
-  a: [1, 2]
+  a: [1, 2],
+  res: {
+    e: undefined,
+    r: {
+      '0': 1,
+      '1': 2
+    }
+  }
 }, {
-  a: '[1,2]'
+  a: '[1,2]',
+  res: {
+    e: undefined,
+    r: {
+      '0': 1,
+      '1': 2
+    }
+  }
 }, {
-  a: 'eyJhIjoxfQ=='
+  a: 'eyJhIjoxfQ==',
+  res: {
+    e: undefined,
+    r: {
+      a: 1
+    }
+  }
 }, {
-  a: 'W29iamVjdCBPYmplY3Rd'
+  a: 'W29iamVjdCBPYmplY3Rd',
+  res: {
+    e: new Error('SyntaxError: Unexpected token o'),
+    r: undefined
+  }
 }];
-for (var i=0; i < tests.length; i++) {
-  console.log(parse(tests[i]['a']));
-}
+
+tape('Check parseJson]', function (t) {
+  for (var i = 0; i < tests.length; i++) {
+    var result = parse(tests[i].a);
+    console.log('result', result);
+    t.deepEqual(result, tests[i].res);
+  }
+  t.end();
+});
