fa9be2987e22966251e03f3e9c2143466902fbc1
Jim McAndrew <jim@loc8.us>
2015-12-31 12:45:37 -0700

    Add more tests

--- src/tests/parseJson.js
+++ src/tests/parseJson.js
@@ -1,6 +1,7 @@
 var tape = require('tape');
 var parse = require('../parseJson.js');
 var tests = [{
+  name: 'Object',
   a: {
     'test': 'one'
   },
@@ -11,6 +12,7 @@ var tests = [{
     }
   }
 }, {
+  name: 'JSON String',
   a: '{"test": "one"}',
   res: {
     e: undefined,
@@ -19,12 +21,14 @@ var tests = [{
     }
   }
 }, {
+  name: 'Invalid String',
   a: 'test',
   res: {
-    e: new Error('SyntaxError: Unexpected token µ'),
+    e: 'Value cannot be made into an object',
     r: undefined
   }
 }, {
+  name: 'Valid Array',
   a: [1, 2],
   res: {
     e: undefined,
@@ -34,6 +38,7 @@ var tests = [{
     }
   }
 }, {
+  name: 'Valid Array as String',
   a: '[1,2]',
   res: {
     e: undefined,
@@ -43,6 +48,7 @@ var tests = [{
     }
   }
 }, {
+  name: 'Invalid ATOB encoded Object as String',
   a: 'eyJhIjoxfQ==',
   res: {
     e: undefined,
@@ -51,17 +57,54 @@ var tests = [{
     }
   }
 }, {
+  name: 'Invalid encoded String',
   a: 'W29iamVjdCBPYmplY3Rd',
   res: {
-    e: new Error('SyntaxError: Unexpected token o'),
+    e: 'Value cannot be made into an object',
     r: undefined
+  }
+}, {
+  name: 'Valid lz-string compress encoded String',
+  a: '㞂₆⁜ࣀ빀',
+  res: {
+    e: undefined,
+    r: {
+      a: 1
+    }
+  }
+}, {
+  name: 'Valid lz-string compressToBase64 encoded String',
+  a: 'N4IghiBcCMC+Q===',
+  res: {
+    e: undefined,
+    r: {
+      a: 1
+    }
+  }
+}, /*{
+  name: 'Valid lz-string compressToUTF16 encoded String',
+  a: '\u1BE1\u0841\u442B\u40AC  ',
+  res: {
+    e: undefined,
+    r: {
+      a: 1
+    }
+  }
+}, */{
+  name: 'Valid lz-string compressToEncodedURIComponent encoded String',
+  a: 'N4IghiBcCMC+Q',
+  res: {
+    e: undefined,
+    r: {
+      a: 1
+    }
   }
 }];
 
 tape('Check parseJson]', function (t) {
   for (var i = 0; i < tests.length; i++) {
     var result = parse(tests[i].a);
-    console.log('result', result);
+    console.log('result for', tests[i].name, '→', result);
     t.deepEqual(result, tests[i].res);
   }
   t.end();
