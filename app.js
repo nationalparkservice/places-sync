c9f5afa76a941bf6589e090453c594fa8aab7321
Jim McAndrew <jim@loc8.us>
2016-01-08 10:03:32 -0700

    Replace app with a simple demo (to be updated)

--- app.js
+++ app.js
@@ -1,69 +1,22 @@
-var dat = require('dat-core');
-// var db = dat('./places-sync');
-var fs = require('fs');
-var http = require('http');
-var source = JSON.parse(fs.readFileSync('sources.json', 'utf8'))[0];
+var config = require('./config');
+var datawrap = require('datawrap');
+config = datawrap.fandlebars.obj(config, global.process);
 
-http.get(source.feature_service + 'query?f=json&layerDefs=0:' + source.unit_code_key + '=\'' + source.unit_code + '\'&outSR=4326', function (response) {
-  response
-    .pipe(fs.createWriteStream('test.json'));
-    /*
-    .pipe(db.createWriteStream({
-      dataset: 'buildings'
-    }));
-    */
-});
+var syncDb = datawrap(config.database.sync_db, config.database.defaults);
 
-  /*
-  .pipe(select('layers[0]'))
-  .pipe(through(function (o, enc, next) {
-    console.log(o);
-    var properties = o.properties;
-    properties.coordinates = o.geometry.coordinates;
-    properties.key = o.properties[source.source_system_key];
-    console.log(properties);
-    next(null, properties);
-  }))
-  */
-  /*
-  .pipe(ldj.serialize())
-  .pipe(dat('./_dat/buildings').createWriteStream({
-    primary: 'key'
-  }));
-  */
+var t = function (sql) {
+  syncDb.runQuery(sql)
+    .then(function (e, r) {
+      console.log(e, r);
+    });
+};
 
-// console.log(stream);
+var testDB = [
+  'CREATE TABLE sourcea (one VARCHAR(10), two SMALLINT);',
+  "INSERT INTO  sourcea VALUES('hello!',10);",
+  "INSERT INTO sourcea VALUES('goodbye', 20);",
+  'SELECT * FROM sourcea ;',
+  'DROP TABLE sourcea ;'
+];
 
-// request(source.feature_service + 'query?layerDefs=0:' + source.unit_code_key + '=\'' + source.unit_code + '\'&outSR=4326&returnGeometry=true&f=json', function (error, response, body) {
-  // if (!error && response.statusCode === 200) {
-    // var features = JSON.parse(body).layers[0].features;
-    // var path = './_temp/' + new Date().getTime() + '.json';
-
-    // console.log(features);
-    // fs.writeFile(path, JSON.stringify(features));
-    // dat.import(path);
-    // dat('./_data/buildings').import(path);
-
-    /*
-    var geojson = {
-      features: [],
-      type: 'FeatureCollection'
-    };
-
-    for (var i = 0; i < features.length; i++) {
-      var feature = arcgisParser.parse(features[i]);
-
-      feature.id = feature.properties[source.source_system_key];
-      geojson.features.push(feature);
-    }
-
-    path = './_temp/' + new Date().getTime() + '.geojson';
-    fs.writeFile(path, JSON.stringify(geojson));
-    dat.import(path);
-    /*
-    setTimeout(function () {
-      fs.unlink(path);
-    }, 3000);
-    */
-  // }
-// });
+t(testDB);
