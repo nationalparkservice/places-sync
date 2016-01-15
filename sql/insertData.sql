416e5c5a71ccbb8e9ca4e1e4c471e2bfa8467bc1
Jim McAndrew <jim@loc8.us>
2016-01-15 14:50:15 -0700

    Update insert Data

---
+++ sql/insertData.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.master (
+  key,
+  process,
+  source,
+  hash,
+  last_update,
+  removed
+) VALUES (
+  '{{key}}',
+  '{{process}}',
+  '{{source}}',
+  '{{hash}}',
+  '{{last_update}}',
+  '{{removed}}'
+);
