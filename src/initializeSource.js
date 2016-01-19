5c6cc08d33da54ae39b36ec8dcd2a347ff4eb88e
Jim McAndrew <jim@loc8.us>
2016-01-19 11:53:14 -0500

    Come up with the merge process

---
+++ src/initializeSource.js
@@ -0,0 +1,16 @@
+module.exports = function (process, sourceA, sourceB) {
+  // Make sure we have the master table
+  // Make sure we don't have any previous data for this process
+  // Maybe ask if we can wipe it?
+  //
+  // import sourceA
+  // import sourceB
+  
+
+};
+
+var importSource = function(process, source) {
+  // TODO there will need to be a tool that makes a source from a database and table
+  
+
+};
