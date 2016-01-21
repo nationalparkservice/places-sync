c32649f5aabbce3986d144649934f6ad177fce78
Jim McAndrew <jim@loc8.us>
2016-01-21 12:35:01 -0500

    Add geojson to table with rows function

---
+++ src/geojsonToJsonTable.js
@@ -0,0 +1,39 @@
+var reproject = require('reproject');
+var epsg = require('proj4js-defs');
+
+module.exports = function (geojson) {
+  var geoJsonObj;
+  var crss = getEpsgDefs();
+  var error;
+
+  // Parse or clone the GeoJSON file
+  geoJsonObj = JSON.parse(typeof geojson === 'string' ? geojson : JSON.stringify(geojson));
+
+  // Reproject the GeoJSON File
+  geoJsonObj = reproject.reproject(geoJsonObj, null, 'EPSG:4326', crss);
+
+  // convert the geojson to rows for the table
+  return geojsonToRows(geoJsonObj);
+};
+
+var getEpsgDefs = function () {
+  var temp = {
+    defs: {}
+  };
+  epsg(temp);
+  return temp.defs;
+};
+
+var geojsonToRows = function (geojson) {
+  var features = geojson.features;
+  var property;
+  var rows = features.map(function (feature) {
+    var properties = {};
+    for (property in feature.properties) {
+      properties[property] = typeof feature.properties[property] !== 'object' ? feature.properties[property] : JSON.stringify(feature.properties[property]);
+    }
+    properties['geometry'] = JSON.stringify(feature.geometry);
+    return properties;
+  });
+  return rows;
+};
