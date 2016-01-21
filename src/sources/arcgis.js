376eb8a510f08740eae762b468f505ad1902ca27
Jim McAndrew <jim@loc8.us>
2016-01-21 12:34:30 -0500

    Prototype what the arcgis plugin will do

--- src/sources/arcgis.js
+++ src/sources/arcgis.js
@@ -1,1 +1,13 @@
-var arcgis = function(layerUrl, 
+var arcgis = function(layerUrl, columns, customFunctions) {
+  // Columns needs to include a primary key, all other columns will be pulled from the layerURL result
+  var taskList = [];
+
+  // Read the layerURL with ?f=pjson at the end if it's not already there
+  // Extract information from this document
+    // Column Names
+  // Query the service
+  // TODO: customFunctions can define to have a different where clause
+  // Go through the JSON and clean it up (may require reprojecting to WGS84)
+  // Create a table
+  // Return the source object
+};
