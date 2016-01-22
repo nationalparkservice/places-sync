29484d74b29e17b80fdad3415f535eb76c4f7bd3
Jim McAndrew <jim@loc8.us>
2016-01-22 14:41:13 -0500

    Support external columns

--- src/oldSrc/jsonToTable.js
+++ src/oldSrc/jsonToTable.js
@@ -1,38 +1,28 @@
-var Bluebird = require('datawrap').Bluebird;
 var tools = require('./tools');
 
-module.exports = function (tableName, data, columns) { // TODO: Support external column types
-  return new Bluebird(function (fulfill, reject) {
-    var jsonInfo = {
-      'name': tableName,
-      'columns': []
-    };
-    var types = {};
-    try {
-      jsonInfo.data = JSON.parse(typeof data === 'string' ? data : JSON.stringify(data));
-    } catch (parseError) {
-      reject(parseError);
-    }
+module.exports = function (tableName, data, predefinedColumns) {
+  // Define the new info object
+  var jsonInfo = {
+    'name': tableName,
+    'columns': predefinedColumns || []
+  };
 
-    if (jsonInfo.data) {
-      jsonInfo.data.forEach(function (row) {
-        for (var column in row) {
-          if (jsonInfo.columns.indexOf(column) === -1) {
-            jsonInfo.columns.push(column);
-            types[column] = types[column] || 'integer';
-          }
-          types[column] = tools.getType(row[column], types[column], typeof row[column]);
-        }
-      });
-      jsonInfo.columns = jsonInfo.columns.map(function (name) {
-        return {
-          'name': name,
-          'type': types[name]
-        };
-      });
-      fulfill(jsonInfo);
+  // Add the data to the object
+  jsonInfo.data = JSON.parse(typeof data === 'string' ? data : JSON.stringify(data));
+
+  // Go through all the columns and determine their data types
+  jsonInfo.data.forEach(function (row) {
+    var thisColumn;
+    for (var column in row) {
+      if (!predefinedColumns) {
+        thisColumn = jsonInfo.columns.filter(function (value) {
+            return value.name === column;
+          })[0] || jsonInfo.columns[jsonInfo.columns.push({
+            'name': column
+          }) - 1];
+        thisColumn.type = tools.getType(row[column], thisColumn.type || 'integer', typeof row[column]);
+      }
     }
   });
+  return jsonInfo;
 };
-
-
