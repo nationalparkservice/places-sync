30801432db124238ed9296816df2b4bdaab3e961
Jim McAndrew <jim@loc8.us>
2016-01-23 16:45:26 -0500

    Update loadData.js

--- src/loadData.js
+++ src/loadData.js
@@ -1,7 +1,11 @@
 var datawrap = require('datawrap');
 var tools = require('./tools');
 
-module.exports = function (tableName, columns, data, options, database) {
+module.exports = function (source, tableData, database) {
+  var tableName = tableData.name;
+  var columns = tableData.columns;
+  var data = tableData.data;
+
   return new datawrap.Bluebird(function (fulfill, reject) {
     var createTable = 'CREATE TABLE "' + tableName + '" (' + columns.map(function (column) {
       return '"' + column.name + '" ' + column.type.toUpperCase();
@@ -33,7 +37,7 @@ module.exports = function (tableName, columns, data, options, database) {
       .then(function (a) {
         fulfill(a);
       }).catch(function (e) {
-        reject(e);
-      });
+      reject(e);
+    });
   });
 };
