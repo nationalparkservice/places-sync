926fdfb847a0e2709cfc3a87c651619078c62b5c
Jim McAndrew <jim@loc8.us>
2016-01-23 16:45:10 -0500

    Update json.js

--- src/formats/json.js
+++ src/formats/json.js
@@ -1,28 +1,28 @@
-var tools = require('./tools');
+var tools = require('../tools');
 
-module.exports = function (tableName, data, predefinedColumns) {
-  // Define the new info object
-  var jsonInfo = {
-    'name': tableName,
-    'columns': predefinedColumns || []
-  };
-
+module.exports = tools.syncPromise(function (source) {
   // Add the data to the object
-  jsonInfo.data = JSON.parse(typeof data === 'string' ? data : JSON.stringify(data));
+  source.data = JSON.parse(typeof source.data === 'string' ? source.data : JSON.stringify(source.data));
+  var predefinedColumns = !!source.columns;
+  source.columns = [];
+  if (typeof source.data === 'string') {
+    source.data = JSON.parse(source.data);
+  }
 
   // Go through all the columns and determine their data types
-  jsonInfo.data.forEach(function (row) {
+  source.data.forEach(function (row) {
     var thisColumn;
     for (var column in row) {
       if (!predefinedColumns) {
-        thisColumn = jsonInfo.columns.filter(function (value) {
-            return value.name === column;
-          })[0] || jsonInfo.columns[jsonInfo.columns.push({
-            'name': column
-          }) - 1];
+        thisColumn = source.columns.filter(function (value) {
+          return value.name === column;
+        })[0] || source.columns[source.columns.push({
+          'name': column
+        }) - 1];
         thisColumn.type = tools.getType(row[column], thisColumn.type || 'integer', typeof row[column]);
       }
     }
   });
-  return jsonInfo;
-};
+
+  return source;
+});
