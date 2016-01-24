b022d5c812fef198d017f9b742709eb8fbd066b1
Jim McAndrew <jim@loc8.us>
2016-01-24 11:26:49 -0500

    Force file to return a string, so we don't need to do it here

--- src/transformData.js
+++ src/transformData.js
@@ -3,7 +3,15 @@ var formats = tools.requireDirectory(__dirname + '/formats');
 var guessFormat = require('./guessFormat');
 
 module.exports = function (source, data) {
-  source.data = data.data || data.toString();
+  if (typeof data === 'object') {
+    // It might have some values we want in our source
+    for (var field in data) {
+      console.log('has field', field);
+      source[field] = data[field];
+    }
+  } else {
+    source.data = data.toString();
+  }
   var dataFormat = source.format || guessFormat(source.data);
   if (formats[dataFormat]) {
     return formats[dataFormat](source);
