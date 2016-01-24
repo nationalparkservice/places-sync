56dc7976692c756a4d8d894dd6f7e4d07a426cea
Jim McAndrew <jim@loc8.us>
2016-01-24 11:25:09 -0500

    Default files to utf8

--- src/extractors/file.js
+++ src/extractors/file.js
@@ -1,5 +1,6 @@
 var fs = require('datawrap').Bluebird.promisifyAll(require('fs'));
 
 module.exports = function (source) {
+  source.fileOptions = source.fileOptions || 'utf8';
   return fs.readFileAsync(source.data, source.fileOptions);
 };
