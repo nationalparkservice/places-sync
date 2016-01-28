252dd862b581e272dd02ce349352aa7d2dd707d0
Jim McAndrew <jim@loc8.us>
2016-01-28 17:33:59 -0500

    Fast dedupe

--- src/tools.js
+++ src/tools.js
@@ -12,6 +12,21 @@ var tools = module.exports = {
   getJsType: function (value) {
     return Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
   },
+  dedupe: function (a) {
+    // http://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
+    var seen = {};
+    var out = [];
+    var len = a.length;
+    var j = 0;
+    for (var i = 0; i < len; i++) {
+      var item = a[i];
+      if (seen[item] !== 1) {
+        seen[item] = 1;
+        out[j++] = item;
+      }
+    }
+    return out;
+  },
   readOutput: function (output) {
     return JSON.stringify(tools.arrayify(output).map(function (o) {
       if (Array.isArray(o)) {
