25b741ba88799ccb634e9f224381e9752433150f
Jim McAndrew <jim@loc8.us>
2016-02-26 16:14:24 -0700

    Update index.js

--- src/sources/index.js
+++ src/sources/index.js
@@ -7,107 +7,111 @@ var tools = require('../tools');
 var sources = tools.requireDirectory(__dirname, [__filename]);
 var createDatabase = require('./helpers/jsonToSqlite');
 
-var SourceObject = function (database, columns, sourceConfig) {
-  // Query based on the primary key(s), or all keys if no primary keys exist
-  var queryKey = tools.Arrayify(sourceConfig.primaryKey || tools.simplifyArray(columns));
+var CreateQueries = function (columns, primaryKey, lastUpdate) {
+  var queryKey = tools.arrayify(primaryKey || tools.simplifyArray(columns));
 
-  // Adds quotes around objects in an array
-  // TODO: make this better
-  var quoteArray = function (a) {
-    // Makes it easy to query all columns
-    return a.map(function (v) {
-      return '"' + v + '"';
-    });
+  var arrayToColumns = function (columns, tableName) {
+    return tools.simplifyArray(columns).map(function (c) {
+      return (tableName ? '"' + tableName + '".' : '') + '"' + c + '"';
+    }).join(',');
   };
 
-  var modify = {
-    'insert': function (tableName, values) {},
-    'remove': function (tableName, values) {}
-  };
-
-  var createWhereObj = function (keys, values) {
-    // The keys must come in as an array
-    // is no keys are passed in, it is assumed that all the columns are keys
-    keys = keys || queryKey;
-    if (!Array.isArray(keys)) {
-      throw new Error('createWhereObj keys must be a string or an array.');
-    }
-
-    // Allow either an array or an object to be passed in for values
-    var valuesObj = values;
+  var arraysToObj = function (keys, values) {
+    // Takes two arrays ['a','b','c'], [1,2,3]
+    // And makes an object {'a':1,'b':2,'c':3}
+    var returnObject = {};
     if (Array.isArray(values)) {
-      valuesObj = {};
-      keys.forEach(function (pk, i) {
-        valuesObj[pk] = values[i];
+      keys.forEach(function (key, i) {
+        returnObject[key] = values[i];
       });
+    } else if (typeof values === 'object') {
+      returnObject = values;
     }
+    return returnObject;
+  };
 
-    // If nothing is specified for a value, we just assume not null
-    var defaultWhere = {
-      '$ne': null
-    };
-
+  var createWhereObj = function (keys, values, defaultWhere) {
+    var valuesObj = arraysToObj(keys, values);
     var whereObj = {};
 
+    // If nothing is specified for a value, the default is (null or not null)
+    defaultWhere = defaultWhere || {
+      '$or': [{
+        'id': {
+          '$eq': null
+        }
+      }, {
+        'id': {
+          '$ne': null
+        }
+      }]
+    };
+
+    // Add the default value where nothing else is
     keys.forEach(function (pk) {
       whereObj[pk] = valuesObj[pk] || defaultWhere;
     });
-    return whereObj;
+
+    console.log(JSON.stringify(whereObj, null, 2));
+    return tools.createWhereClause(whereObj);
   };
 
-  var quotedColumns = function (tableName) {
-    return quoteArray(tools.simplifyArray(columns)).map(function (c) {
-      return '"' + tableName + '".' + c;
-    }).join(',');
+  var queries = {
+    'selectAll': function () {
+      var selectAllQuery = 'SELECT ' + arrayToColumns(columns, 'all_data') + ' FROM (';
+      selectAllQuery += ' SELECT ' + arrayToColumns(columns, 'source');
+      selectAllQuery += ' FROM "source"';
+      selectAllQuery += ' LEFT JOIN "remove" ON ' + queryKey.map(function (pk) {
+        return '"remove"."' + pk + '" = "source"."' + pk + '"';
+      }).join(' AND ');
+      selectAllQuery += ' LEFT JOIN  "new" ON ' + queryKey.map(function (pk) {
+        return '"new".' + pk + ' = "source"."' + pk + '"';
+      }).join(' AND ');
+      selectAllQuery += ' WHERE';
+      selectAllQuery += queryKey.map(function (pk) {
+        return '"remove"."' + pk + '" IS NULL';
+      }).join(' AND ');
+      selectAllQuery += ' AND ';
+      selectAllQuery += queryKey.map(function (pk) {
+        return '"new"."' + pk + '" IS NULL';
+      }).join(' AND ');
+      selectAllQuery += ' UNION';
+      selectAllQuery += ' SELECT ' + arrayToColumns(columns, 'new');
+      selectAllQuery += ' FROM "new") AS "all_data"';
+      return selectAllQuery;
+    },
+    'selectLastUpdate': function () {
+      if (lastUpdate) {
+        return 'SELECT MAX("all_data"."' + lastUpdate + '" AS "lastUpdate") FROM ' + queries.selectAllQuery + ') AS "last_update"';
+      } else {
+        return 'SELECT 0 AS "lastUpdate" ';
+      }
+    }
   };
-  var quotedPrimaryKeys = quoteArray(tools.arrayify(sourceConfig.primaryKey || tools.simplifyArray(columns)));
+  return function (queryName, values, keys) {
+    var where = createWhereObj(tools.simplifyArray(keys || queryKey), values);
+    var query = queries[queryName]() + ' WHERE ' + where[0];
+    console.log(where);
+    return [query, where[1]];
+  };
+};
 
-  var selectAllQuery = 'SELECT ' + quotedColumns('all_data') + ' FROM (';
-  selectAllQuery += ' SELECT ' + quotedColumns('source');
-  selectAllQuery += ' FROM "source"';
-  selectAllQuery += ' LEFT JOIN "remove" ON ' + quotedPrimaryKeys.map(function (pk) {
-    return '"remove".' + pk + ' = "source".' + pk;
-  }).join(' AND ');
-  selectAllQuery += ' LEFT JOIN  "new" ON ' + quotedPrimaryKeys.map(function (pk) {
-    return '"new".' + pk + ' = "source".' + pk;
-  }).join(' AND ');
-  selectAllQuery += ' WHERE';
-  selectAllQuery += quotedPrimaryKeys.map(function (pk) {
-    return '"remove".' + pk + ' IS NULL';
-  }).join(' AND ');
-  selectAllQuery += ' AND ';
-  selectAllQuery += quotedPrimaryKeys.map(function (pk) {
-    return '"new".' + pk + ' IS NULL';
-  }).join(' AND ');
-  selectAllQuery += ' UNION';
-  selectAllQuery += ' SELECT ' + quotedColumns('new');
-  selectAllQuery += ' FROM "new") AS "all_data"';
+var SourceObject = function (database, columns, sourceConfig) {
+  // Query based on the primary key(s), or all keys if no primary keys exist
+  var createQueries = new CreateQueries(columns, sourceConfig.primaryKey, sourceConfig.lastUpdate);
 
   var actions = {
     'selectAll': function (whereObj) {
       // Selects all fields, if not whereObj is supplied, it will query everything
-      whereObj = whereObj || createWhereObj(queryKey);
-      var where = tools.createWhereClause(whereObj);
-      var query = selectAllQuery + ' WHERE ' + where[0] + ';';
-      var params = where[1];
-      return database.query(query, params);
+      return database.query.apply(this, createQueries('selectAll', whereObj, columns));
     },
-    'selectOne': function (queryKeyValues) {
+    'selectLastUpdate': function (whereObj) {
+      // gets the max date from the lastUpdate field, a whereObj can be applied to this
       return new Promise(function (fulfill, reject) {
-        if (!queryKeyValues) {
-          reject(new Error('selectOne requires the values of the primary keys in an array'));
-        } else if (!sourceConfig.primaryKey) {
-          reject(new Error('Not primary keys are defined'));
-        }
-        queryKeyValues = tools.arrayify(queryKeyValues);
-        var whereObj = tools.createWhereObj(queryKey, queryKeyValues);
-        actions.selectAll(whereObj).then(function (r) {
-          return r[0];
+        database.query.apply(this, createQueries('selectLastUpdate', whereObj)).then(function (r) {
+          fulfill(r[0].lastUpdate);
         }).catch(reject);
       });
-    },
-    'selectLastUpdateTime': function (whereObj) {
-      // gets the max date from the lastUpdate field, a whereObj can be applied to this
     },
     'describe': function () {
       // Returns the columns and their data types
@@ -145,6 +149,7 @@ var SourceObject = function (database, columns, sourceConfig) {
 
   return actions;
 };
+
 module.exports = function (sourceConfig, lastUpdate) {
   var source = sources[sourceConfig.connection && sourceConfig.connection.type];
   if (!source) {
@@ -157,7 +162,7 @@ module.exports = function (sourceConfig, lastUpdate) {
         'task': sources[sourceConfig.connection.type],
         'params': [sourceConfig]
       }, {
-        'name': 'Create Database',
+        'name ': 'Create Database',
         'description': 'Creates a database from the JSON representation of the data and the columns',
         'task': createDatabase,
         'params': ['{{dataToJson.data}}', '{{dataToJson.columns}}', sourceConfig]
