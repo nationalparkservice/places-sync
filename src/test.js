cfcaa375d2d95561a164be104107fc50fbbfca32
Jim McAndrew <jim@loc8.us>
2016-02-26 12:36:11 -0700

    Test more

--- src/test.js
+++ src/test.js
@@ -7,7 +7,13 @@ var csvConfig = {
     'type': 'csv'
   }
 };
+var thrower = function (e) {
+  throw e;
+};
 
-sources(csvConfig).then(console.log).catch(function (e) {
+sources(csvConfig).then(function (source) {
+  console.log(source);
+  source.selectAll().then(console.log).catch(thrower);
+}).catch(function (e) {
   throw Array.isArray(e) ? e[e.length - 1] : e;
 });
