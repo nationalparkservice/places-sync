bed85c7133868e91b8c8c59a89943a0edec7d3ff
Jim McAndrew <jim@loc8.us>
2016-04-07 11:53:17 -0600

    Reformat

--- src/sources/helpers/jsonToSqlite.js
+++ src/sources/helpers/jsonToSqlite.js
@@ -2,7 +2,7 @@ var tools = require('../../tools');
 var Promise = require('bluebird');
 var database = require('../../databases');
 
-var getColumnInfo = function(data, existingColumns) {
+var getColumnInfo = function (data, existingColumns) {
   // Figure out what the columns are
   // Fill in any values that we didn't have before
   // If there are defined columns, use only columns that arent in the defined list
@@ -10,7 +10,7 @@ var getColumnInfo = function(data, existingColumns) {
   var defaultValues = {};
   // Try to read column info from the data, if data exists, otherwise use the existing columns
   if (data) {
-    data.forEach(function(row) {
+    data.forEach(function (row) {
       for (var column in row) {
         var columnIndex = tools.simplifyArray(newColumns).indexOf(column) > -1 ? tools.simplifyArray(newColumns).indexOf(column) : newColumns.push({
           'name': column,
@@ -31,7 +31,7 @@ var getColumnInfo = function(data, existingColumns) {
       }
     });
     // Add in the default values if there are any
-    newColumns = newColumns.map(function(c) {
+    newColumns = newColumns.map(function (c) {
       if (defaultValues[c.name].hasDefault === true) {
         c.defaultValue = defaultValues[c.name].firstValue;
       }
@@ -43,8 +43,8 @@ var getColumnInfo = function(data, existingColumns) {
 
   // If there defined columns, use only columns that aren't in the defined list
   if (existingColumns) {
-    newColumns = existingColumns.map(function(c) {
-      var matchedColumn = newColumns.filter(function(nc) {
+    newColumns = existingColumns.map(function (c) {
+      var matchedColumn = newColumns.filter(function (nc) {
         return nc.name === c.name;
       })[0] || {};
       for (var k in matchedColumn) {
@@ -56,8 +56,8 @@ var getColumnInfo = function(data, existingColumns) {
   return newColumns;
 };
 
-module.exports = function(data, existingColumns) {
-  return new Promise(function(fulfill, reject) {
+module.exports = function (data, existingColumns) {
+  return new Promise(function (fulfill, reject) {
     var columns = getColumnInfo(data, existingColumns);
 
     var tempDbConfig = {
@@ -65,12 +65,12 @@ module.exports = function(data, existingColumns) {
       'connection': ':memory:'
     };
 
-    var createColumns = function(includeColumns) {
+    var createColumns = function (includeColumns) {
       var primaryKeys = [];
-      var columnArray = includeColumns.map(function(column) {
+      var columnArray = includeColumns.map(function (column) {
         var createColumn = tools.surroundValues(column.name, '"') + ' ' + column.sqliteType;
         if (column.defaultValue !== undefined) {
-          createColumn += ' DEFAULT ' + tools.surroundValues(tools.normalizeToType(column.defaultValue), typeof tools.normalizeToType(column.defaultValue) === 'string' ? "'" : "") + ' ';
+          createColumn += ' DEFAULT ' + tools.surroundValues(tools.normalizeToType(column.defaultValue), typeof tools.normalizeToType(column.defaultValue) === 'string' ? "'" : '') + ' ';
         } else if (column.notNull === true) {
           createColumn += ' NOT NULL ';
         }
@@ -89,15 +89,16 @@ module.exports = function(data, existingColumns) {
     var createStatementCache = 'CREATE TABLE "cached" ' + createColumns(columns);
     var createStatementUpdate = 'CREATE TABLE "updated" ' + createColumns(columns);
     var createStatementRemove = 'CREATE TABLE "removed" ' + createColumns(columns);
+    var createStatementMetadata = 'CREATE TABLE "metadata" ("key" TEXT, "foreignKey" TEXT, "lastUpdated" NUMERIC, "hash" text, "data" BLOB, "action" TEXT, PRIMARY KEY ("key"));';
 
     // console.log(createStatementCache);
     // console.log(createStatementUpdate);
     // console.log(createStatementRemove);
     // Create an 'INSERT INTO' statement for the sqlite database
     // TODO, use the createQueries tool
-    var insertStatement = 'INSERT INTO "cached" (' + tools.simplifyArray(columns).map(function(c) {
+    var insertStatement = 'INSERT INTO "cached" (' + tools.simplifyArray(columns).map(function (c) {
       return '"' + c + '"';
-    }).join(', ') + ') VALUES (' + tools.simplifyArray(columns).map(function(c) {
+    }).join(', ') + ') VALUES (' + tools.simplifyArray(columns).map(function (c) {
       return '{{' + c + '}}';
     }).join(', ') + ')';
 
@@ -119,11 +120,15 @@ module.exports = function(data, existingColumns) {
       'task': '{{Create Database.query}}',
       'params': [createStatementRemove]
     }, {
+      'name': 'Create Metadata Table',
+      'task': '{{Create Database.query}}',
+      'params': [createStatementMetadata]
+    }, {
       'name': 'Insert Data',
       'task': '{{Create Database.queryList}}',
       'params': [insertStatement, tools.arrayify(data)]
     }];
-    tools.iterateTasks(taskList, 'jsonToSqlite', false).then(function(a) {
+    tools.iterateTasks(taskList, 'jsonToSqlite', false).then(function (a) {
       fulfill(tools.arrayGetLast(a, true));
     }).catch(reject);
   });
