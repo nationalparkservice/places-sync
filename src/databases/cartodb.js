32e248b1a96d546ec28aa00beb68886b045ea13e
Jim McAndrew <jim@loc8.us>
2016-04-15 06:04:10 -0600

    Get cartodb sync working

--- src/databases/cartodb.js
+++ src/databases/cartodb.js
@@ -1,23 +1,68 @@
 var Promise = require('bluebird');
 var superagent = require('superagent');
 var fandlebars = require('fandlebars');
+var tools = require('../tools');
 
 var format = function (output) {
   // TODO: Clean up the output from this so it's similar to all other outputs
   var result = [];
-  if (output && output.text && output.text.rows) {
-    result = output.text.rows;
-    result.fields = output.text.fields;
+  if (output && output.body && output.body.rows) {
+    result = output.body.rows;
+    result.fields = output.body.fields;
   }
   return result;
 };
 
+var parameterize = function (param, type) {
+  // CartoDB doesn't really support parameterized queries, so we'll do them our own way
+  param = tools.normalizeToType(param);
+  if (param === null || param === undefined) {
+    return 'null';
+  } else {
+    param = param.toString();
+  }
+  var returnValue = "convert_from(decode('";
+  type = type || tools.getDataType(param);
+  if (type !== 'text') {
+    returnValue = "'" + param.replace(/\'/g, "''") + "'::" + type;
+  } else {
+    for (var i = 0; i < param.length; i++) {
+      returnValue += param.charCodeAt(i).toString(16);
+    }
+    returnValue += "', 'hex'), 'UTF-8')::" + type;
+  }
+
+  return returnValue;
+};
+var parameterizeQuery = function (query, params, columns) {
+  params = JSON.parse(JSON.stringify(params));
+  for (var column in params) {
+    var matchedColumn = columns && columns.filter(function (c) {
+      return c.name === column;
+    })[0];
+    params[column] = parameterize(params[column], matchedColumn && !matchedColumn.transformed && matchedColumn.type);
+  }
+  return fandlebars(query, params);
+};
+
 module.exports = function (connectionConfig) {
   var returnObject = {
-    query: function (query, params, returnRaw) {
+    query: function (query, params, returnRaw, columns) {
       return new Promise(function (fulfill, reject) {
-        var cleanedSql = fandlebars(query, params);
-        var requestPath = 'https://' + connectionConfig.account + '.cartodb.com/api/v2/sql';
+        // var cleanedSql = fandlebars(query, params);
+        var cleanedSql = parameterizeQuery(query, params, columns);
+        var requestPath = 'https://' + connectionConfig.connection.account + '.cartodb.com/api/v2/sql';
+        console.log('|-cleanedSQL---------------------');
+        console.log(cleanedSql);
+        console.log('|-query--------------------------');
+        console.log(query);
+        console.log('|-params-------------------------');
+        console.log(params);
+        console.log('|-returnRaw------------------------');
+        console.log(returnRaw);
+        console.log('|-columns------------------------');
+        console.log(columns);
+        console.log('|--------------------------------');
 
         if (cleanedSql.length > 5) {
           superagent.post(requestPath)
@@ -25,7 +70,7 @@ module.exports = function (connectionConfig) {
             .set('Accept', 'application/json')
             .send({
               'q': cleanedSql,
-              'api_key': connectionConfig.apiKey
+              'api_key': connectionConfig.connection.apiKey
             })
             .end(function (err, response) {
               if (err || response.error) {
