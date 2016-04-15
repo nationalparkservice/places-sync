6c7d36abb0792c9fab949affe7a57df3caf1865b
Jim McAndrew <jim@loc8.us>
2016-04-15 06:04:37 -0600

    Get cartodb sync working

--- src/sources/cartodb.js
+++ src/sources/cartodb.js
@@ -6,31 +6,45 @@ var Immutable = require('immutable');
 var tools = {
   'iterateTasks': require('../tools/iterateTasks'),
   'simplifyArray': require('../tools/simplifyArray'),
+  'setProperty': require('../tools/setProperty'),
   'dummyPromise': require('../tools/dummyPromise')
 };
 var CreateQueries = require('./helpers/createQueries');
 var columnsFromConfig = require('./helpers/columnsFromConfig');
 var columnsToKeys = require('./helpers/columnsToKeys');
+var mapColumns = require('./helpers/mapColumns');
 var databases = require('../databases');
 var getSqliteType = require('./helpers/getSqliteTypeFromPg');
 
-var QuerySource = function (connection, options, tableName, columns) {
+var QuerySource = function (connection, options, account, tableName, columns) {
   return function (type, whereObj, returnColumns) {
     returnColumns = returnColumns || columns;
-    var keys = columnsToKeys(returnColumns);
-    var createQueries = new CreateQueries(columns, keys.primaryKeys, keys.lastUpdatedField, keys.removedField);
-    return connection.query.apply(this, createQueries(type, whereObj, returnColumns, tableName));
+    var unmappedColumns = mapColumns.from(columns, unmappedColumns);
+    var keys = columnsToKeys(unmappedColumns);
+    var createQueries = new CreateQueries(unmappedColumns, keys.primaryKeys, keys.lastUpdatedField, keys.removedField, options);
+    // TODO createQueries should take a schema
+    return connection.query.apply(this, createQueries(type, whereObj, returnColumns, tableName).concat([false, unmappedColumns]));
   };
 };
 
-var WriteFn = function (connection, options, tableName, columns) {
-  var keys = columnsToKeys(columns);
-  var createQueries = new CreateQueries(columns, keys.primaryKeys);
+var WriteFn = function (connection, options, account, tableName, columns) {
+  var unmappedColumns = mapColumns.from(columns, unmappedColumns);
+  var keys = columnsToKeys(unmappedColumns);
+  var createQueries = new CreateQueries(unmappedColumns, keys.primaryKeys, keys.lastUpdatedField, keys.removedField, options);
+
+  // TODO: put this somewhere better
+  if (options && options.transforms) {
+    for (var transform in options.transforms) {
+      var columnIdx = tools.simplifyArray(unmappedColumns).indexOf(transform);
+      if (columnIdx > -1) {
+        unmappedColumns[columnIdx].transformed = true;
+      }
+    }
+  }
 
   return function (updated, removed) {
     var tasks = [];
     var removeTasks = [];
-    var keys = columnsToKeys(columns);
     if (keys.removedFieldValue !== undefined) {
       removed.forEach(function (row) {
         row[keys.removedField] = keys.removedFieldValue;
@@ -46,11 +60,11 @@ var WriteFn = function (connection, options, tableName, columns) {
           [{
             'name': 'Remove',
             'task': connection.query,
-            'params': createQueries('remove', updatedRow, keys.primaryKeys, tableName)
+            'params': createQueries('remove', updatedRow, keys.primaryKeys, tableName).concat([false, unmappedColumns])
           }, {
             'name': 'Write',
             'task': connection.query,
-            'params': [createQueries('insert', undefined, columns, tableName)[0], updatedRow]
+            'params': [createQueries('insert', undefined, unmappedColumns, tableName)[0], updatedRow, false, unmappedColumns]
           }],
           'update cartodb db', true
         ]
@@ -60,7 +74,7 @@ var WriteFn = function (connection, options, tableName, columns) {
       removeTasks.push({
         'name': 'Remove Removed Row ' + i,
         'task': connection.query,
-        'params': createQueries('remove', removedRow, keys.primaryKeys, tableName)
+        'params': createQueries('remove', removedRow, keys.primaryKeys, tableName).concat([false, unmappedColumns])
       });
     });
 
@@ -79,11 +93,10 @@ var WriteFn = function (connection, options, tableName, columns) {
   };
 };
 
-var readCartoDB = function (connection, options, tableName, tableSchema) {
-  tableSchema = tableSchema || 'public';
+var readCartoDB = function (connection, options, config) {
   var queryObj = {
-    'tableSchema': tableSchema,
-    'tableName': tableName
+    'tableSchema': config.account,
+    'tableName': config.tableName
   };
   var queries = [];
 
@@ -91,28 +104,30 @@ var readCartoDB = function (connection, options, tableName, tableSchema) {
   var query = '';
   query += 'SELECT';
   query += '  cdb_columnnames AS "name",';
-  query += "  cdb_columntype('" + connection.account + '.' + tableName + "',";
+  query += "  cdb_columntype('" + config.account + '.' + config.tableName + "',";
   query += '  cdb_columnnames) AS "pgtype"';
   query += 'FROM';
   query += "  cdb_columnnames('";
-  query += connection.account + '.' + tableName + "');";
+  query += config.account + '.' + config.tableName + "');";
   queries.push(query);
 
   // Since the user defined fields aren't available in columnNames, we run this query to get them
-  queries.push('SELECT * FROM "' + connection.account + '"."' + tableName + '" LIMIT 0;');
+  queries.push('SELECT * FROM "' + config.account + '"."' + config.tableName + '" LIMIT 0;');
 
   // To get if fields are nullable, and other info, we load this too
   query = '';
   query += 'SELECT ';
-  query += '"column_name, "column_default", "is_nullable", "ordinal_position" ';
-  query += "FROM CDB_TableIndexes('" + connection.account + '.' + tableName + "');";
+  query += '"column_name", "column_default", "is_nullable", "ordinal_position", "data_type" ';
+  query += 'FROM "information_schema"."columns" ';
+  query += 'WHERE "table_schema" = \'' + config.account + '\' AND "table_name" = \'' + config.tableName + "' ";
+  query += 'ORDER BY "ordinal_position";';
   queries.push(query);
 
   // We need to run this to get the primary keys, since nothing else has them
   query = '';
   query += 'SELECT distinct ';
   query += 'unnest("index_keys") AS "name" ';
-  query += "FROM CDB_TableIndexes('" + connection.account + '.' + tableName + "') ";
+  query += "FROM CDB_TableIndexes('" + config.account + '.' + config.tableName + "') ";
   query += 'WHERE index_primary = true;';
   queries.push(query);
 
@@ -135,19 +150,19 @@ var readCartoDB = function (connection, options, tableName, tableSchema) {
 
         // Get the row that matches from the infoSchema
         var matchedInfoSchema = columnInfoSchema[tools.simplifyArray(columnInfoSchema, 'column_name').indexOf(column.name)] || {};
-
         return {
           'name': column.name,
           'type': type,
           'sqliteType': getSqliteType(type),
           'primaryKey': pkeys.indexOf(column.name) > -1,
-          'defaultValue': typeof matchedInfoSchema.column_default === 'object' ? undefined : matchedInfoSchema.column_default,
+          'defaultValue': undefined, // typeof matchedInfoSchema.column_default === 'object' ? undefined : matchedInfoSchema.column_default,
           'notNull': matchedInfoSchema.is_nullable === 'NO',
           'columnId': matchedInfoSchema.ordinal_position
         };
       });
+
       fulfill({
-        'data': data,
+        'data': undefined,
         'columns': columns
       });
     }).catch(reject);
@@ -158,12 +173,14 @@ module.exports = function (sourceConfig, options) {
   return new Promise(function (fulfill, reject) {
     // Clean up the connectionConfig, and set the defaults
     var connectionConfig = new Immutable.Map(sourceConfig.connection);
-    if (typeof connectionConfig.get('filePath') !== 'string') {
-      throw new Error('filePath must be defined for a SQLite file');
-    }
-    if (typeof (connectionConfig.get('table')) !== 'string') {
-      throw new Error('A table name must be set in the connection for a SQLite file');
-    }
+    var requirements = ['account', 'apiKey', 'tableName'];
+    requirements.forEach(function (requirement) {
+      if (typeof connectionConfig.get(requirement) !== 'string') {
+        throw new Error(requirement + ' must be defined for a CartoDB connection');
+      }
+    });
+    options = options || {};
+    options.transforms = options.transforms || {};
 
     // Define the taskList
     var tasks = [{
@@ -172,21 +189,28 @@ module.exports = function (sourceConfig, options) {
       'task': databases,
       'params': [{
         'type': 'cartodb',
-        'connection': sourceConfig.connection
+        'connection': connectionConfig.toObject()
       }]
     }, {
       'name': 'convertFromTable',
       'description': 'Takes the source data and creates an update/remove cartodb table in memory for it',
       'task': readCartoDB,
-      'params': ['{{createDbConnection}}', options, connectionConfig.get('table'), connectionConfig.get('schema')]
+      'params': ['{{createDbConnection}}', options, connectionConfig.toObject()]
     }];
     tools.iterateTasks(tasks, 'cartodb').then(function (r) {
       var columns = columnsFromConfig(r.convertFromTable.columns, sourceConfig.fields);
+      var mappedColumns = mapColumns.to(columns);
+      var keys = columnsToKeys(columns);
+      options.transforms[keys.lastUpdatedField] = options.transforms[keys.lastUpdatedField] || {
+        'from': ['(EXTRACT(EPOCH FROM ', ')) * 1000'],
+        'to': ["(TIMESTAMP 'epoch' + ", " * INTERVAL '1 millisecond') AT TIME ZONE 'GMT'"]
+      };
+
       fulfill({
         'data': r[1].data,
-        'columns': columns,
-        'writeFn': new WriteFn(r[0], options, connectionConfig.get('table'), columns),
-        'querySource': new QuerySource(r[0], options, connectionConfig.get('table'), columns)
+        'columns': mappedColumns,
+        'writeFn': new WriteFn(r[0], options, connectionConfig.get('account'), connectionConfig.get('tableName'), mappedColumns),
+        'querySource': new QuerySource(r[0], options, connectionConfig.get('account'), connectionConfig.get('tableName'), mappedColumns)
       });
     }).catch(reject);
   });
