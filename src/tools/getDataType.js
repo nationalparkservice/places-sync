240832b573e1ac552d626a5a5cbc9e397943794e
Jim McAndrew <jim@loc8.us>
2016-04-20 16:10:48 -0600

    add bigint

--- src/tools/getDataType.js
+++ src/tools/getDataType.js
@@ -1,5 +1,5 @@
 module.exports = function (value, maxType, dataType) {
-  var typeHierarchy = ['text', 'float', 'integer'];
+  var typeHierarchy = ['text', 'float', 'bigint', 'integer'];
   var passes = 0;
   var type;
   value = value && value.toString ? value.toString() : value;
@@ -7,6 +7,9 @@ module.exports = function (value, maxType, dataType) {
     passes += 1; // this might be a float
     if (parseFloat(value, 10) === parseInt(value, 10) && maxType !== 'float') {
       passes += 1; // This is an int!
+      if (Math.abs(parseInt(value, 10)) < 2147483646) {
+        passes += 1;
+      }
     }
   }
   type = typeHierarchy[passes];
