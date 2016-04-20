1337319b10740fd29e110cb1c7dafc44472275e2
Jim McAndrew <jim@loc8.us>
2016-04-20 16:10:25 -0600

    y

--- src/sources/helpers/mapColumns.js
+++ src/sources/helpers/mapColumns.js
@@ -5,7 +5,7 @@ var map = function (columns, mapField) {
     return c[mapField] !== null;
   }).map(function (c) {
     if (c[mapField] || c[mapField] === 0) {
-      c[(mapFields.indexOf(mapField) + 1) % mapFields.length] = c.name;
+      c[mapFields[(mapFields.indexOf(mapField) + 1) % mapFields.length]] = c.name;
       c.name = c[mapField];
     }
     return c;
@@ -14,9 +14,11 @@ var map = function (columns, mapField) {
 
 module.exports = {
   'to': function (columns) {
+    console.log('to');
     return map(columns, mapFields[0]);
   },
   'from': function (columns) {
+    console.log('from');
     return map(columns, mapFields[1]);
   }
 };
